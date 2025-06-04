a037ae521abd60139d32aa6e9f0414f3e45dba0e
hotboxes <hotboxeslive@gmail.com>
2025-06-03 21:26:54 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     eslint: { ignoreDuringBuilds: true },
     typescript: { ignoreBuildErrors: true },
-    experimental: { missingSuspenseWithCSRBailout: false }
+    output: 'standalone'
   }
