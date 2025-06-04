878e106a0fd23433ef88c6507a1660b07c8fb72e
hotboxes <hotboxeslive@gmail.com>
2025-06-04 17:04:48 -0400

    Update route.ts

--- src/app/api/auth/signout/route.ts
+++ src/app/api/auth/signout/route.ts
@@ -1,7 +1,11 @@
 import { supabase } from '@/lib/supabase';
   import { NextResponse } from 'next/server';
 
-  export async function GET() {
+  export async function GET(request: Request) {
     await supabase.auth.signOut();
-    return NextResponse.redirect(new URL('/', 'https://www.playhotboxes.com'));
+
+    const url = new URL(request.url);
+    const origin = url.origin;
+
+    return NextResponse.redirect(new URL('/', origin));
   }
